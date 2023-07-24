c9b32d9d4cd883e464ed786ab305365064efa1fb
Fabian Ruettgers <fabulon78@gmail.com>
2023-07-24 11:56:44 +0200

    finished

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,45 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=0");
+  const [nextUrl, setNextUrl] = useState("");
+  const [previousUrl, setPreviousUrl] = useState("");
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(url);
         const data = await response.json();
         setPokemon(data.results);
+        setNextUrl(data.next);
+        setPreviousUrl(data.previous);
+        console.log(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [url]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        onClick={() => {
+          setUrl(previousUrl);
+        }}
+        type="button"
+      >
+        Previous Page
+      </button>
+      <button
+        onClick={() => {
+          setUrl(nextUrl);
+        }}
+        type="button"
+      >
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
